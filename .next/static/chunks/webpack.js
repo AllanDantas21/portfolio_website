22e7ce4a371e402edd2853607b0bbe1a69946a09
AllanDantas21 <allandantas21.ad@gmail.com>
2024-04-26 02:11:24 -0300

    animate toggle theme button

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ff8b48f0f003378"; }
+/******/ 		__webpack_require__.h = function() { return "78c85cb49746617f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
