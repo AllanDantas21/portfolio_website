e3b3f01264971aa2bfd21ffc6f97b189200f819f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-01-18 00:05:13 -0300

    add index.js in components to reduce imports

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Layout } from './layouts/article'
+export { default as Section } from './section'
+export { default as Paragraph } from './paragraph'
+export { BioSection, BioYear } from './bio'
