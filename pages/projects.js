c667c83b18918dff49c3c7b0e1e4ef3a01bf2e3f
allandantas21 <allandantas21.ad@gmail.com>
2025-01-18 13:23:13 -0300

    consume projects from constants

--- pages/projects.js
+++ pages/projects.js
@@ -3,22 +3,11 @@ import dynamic from 'next/dynamic'
 import { memo } from 'react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import { projects } from '../data/projects'
 
 const Layout = dynamic(() => import('../components/layouts/article'), {
     ssr: true
 })
-
-import imgFdf from '../public/images/content/fdf.png'
-import imgPushswap from '../public/images/content/pushswap.png'
-import imgPhilo from '../public/images/content/philo.png'
-import imgMinishell from '../public/images/content/minishell.png'
-
-const projects = [
-    { id: 'fdf', title: 'Fil de Fer', thumbnail: imgFdf, description: 'This project consists of graphically creating a schematic representation of a terrain in relief.' },
-    { id: 'philo', title: 'philosophers', thumbnail: imgPhilo, description: 'The famous Dining Philosophers problem, all with the same basic rules.' },
-    { id: 'push-swap', title: 'Push Swap', thumbnail: imgPushswap, description: 'Is a project that involves sorting a list of integers using a limited set of operations.' },
-    { id: 'minishell', title: 'Minishell', thumbnail: imgMinishell, description: 'This project is about creating a simple shell. Yes, a little bash.' }
-]
 
 const Projects = () => (
     <Layout title="Projects">
