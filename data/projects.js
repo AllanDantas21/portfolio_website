566c52d286ed27cd975c3740f8f7fe957dde1788
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-04 01:16:48 -0300

    feat: add i18n integrations in projects page

--- data/projects.js
+++ data/projects.js
@@ -4,8 +4,8 @@ import imgPhilo from '../public/images/content/philo.png'
 import imgMinishell from '../public/images/content/minishell.png'
 
 export const PROJECTS = [
-    { id: 'fdf', title: 'Fil de Fer', thumbnail: imgFdf, description: 'This project consists of graphically creating a schematic representation of a terrain in relief.' },
-    { id: 'philo', title: 'philosophers', thumbnail: imgPhilo, description: 'The famous Dining Philosophers problem, all with the same basic rules.' },
-    { id: 'push-swap', title: 'Push Swap', thumbnail: imgPushswap, description: 'Is a project that involves sorting a list of integers using a limited set of operations.' },
-    { id: 'minishell', title: 'Minishell', thumbnail: imgMinishell, description: 'This project is about creating a simple shell. Yes, a little bash.' }
+    { id: 'fdf', thumbnail: imgFdf },
+    { id: 'philo', thumbnail: imgPhilo },
+    { id: 'push-swap', thumbnail: imgPushswap },
+    { id: 'minishell', thumbnail: imgMinishell }
 ]
--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,8 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 import { memo } from 'react'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import { PROJECTS } from '../data/projects.js'
@@ -9,23 +11,40 @@ const Layout = dynamic(() => import('../components/layouts/article'), {
     ssr: true
 })
 
-const Projects = () => (
-    <Layout title="Projects">
-        <Container>
-            <Heading as="h3" fontSize="2xl" mb={6}>
-                Ecole 42 Projects
-            </Heading>
-            <SimpleGrid columns={[1, 1, 2]} gap={8}>
-                {PROJECTS.map(project => (
-                    <Section delay={0.1} key={project.id}>
-                        <WorkGridItem {...project} thumbnailSize="200px">
-                            {project.description}
-                        </WorkGridItem>
-                    </Section>
-                ))}
-            </SimpleGrid>
-        </Container>
-    </Layout>
-)
+const Projects = () => {
+    const { t } = useTranslation('common')
+
+    return (
+        <Layout title={t('projectsTitle')}>
+            <Container>
+                <Heading as="h3" fontSize="2xl" mb={6}>
+                    {t('ecole42Projects')}
+                </Heading>
+                <SimpleGrid columns={[1, 1, 2]} gap={8}>
+                    {PROJECTS.map(project => (
+                        <Section delay={0.1} key={project.id}>
+                            <WorkGridItem
+                                id={project.id}
+                                title={t(`projects.${project.id}.title`)}
+                                thumbnail={project.thumbnail}
+                                thumbnailSize="200px"
+                            >
+                                {t(`projects.${project.id}.description`)}
+                            </WorkGridItem>
+                        </Section>
+                    ))}
+                </SimpleGrid>
+            </Container>
+        </Layout>
+    )
+}
 
 export default memo(Projects)
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common']))
+        }
+    };
+}
