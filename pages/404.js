cd184f922bf0626710b8429f9f37b34d0d052f4c
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-04 02:17:58 -0300

    feat: implement i18n in not found page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,6 @@
 import NextLink from 'next/link'
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import {
   Box,
   Heading,
@@ -9,18 +11,27 @@ import {
 } from '@chakra-ui/react'
 
 const NotFound = () => {
+  const { t } = useTranslation('common');
   return (
     <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
+      <Heading as="h1">{t('notFound.title', 'Not found')}</Heading>
+      <Text>{t('notFound.description', "The page you're looking for was not found.")}</Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <Button as={NextLink} href="/" colorScheme="teal">
-          Return to home
+          {t('notFound.home', 'Return to home')}
         </Button>
       </Box>
     </Container>
   )
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common']))
+    }
+  };
 }
 
 export default NotFound
